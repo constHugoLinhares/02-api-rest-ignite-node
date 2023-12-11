ec19bed3ab03aafa9ea11d0d2e9b7e9a1955a06c
Hugo Linhares <hugolinhares92@gmail.com>
2023-12-11 19:55:33 -0300

    Teste com o código da aula

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
+import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
 
-app.get('/hello', () => {
-    console.log('hello')
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
